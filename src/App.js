60ce282cac0e70d63de84ff0625b228b037f0b2a
AMPLIFY-AWS <alex1232874@outlook.com>
2024-06-26 08:43:04 -0400

    update to app.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          <code>Chicken door status:</code> OPEN
-        </p>
-
-        <p>Weather status: OVERCAST</p>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+const childProcess = require('child_process');
+const pythonProcess = childProcess.spawn('python', ['your_python_script.py']);
+pythonProcess.stdout.on('data', (data) => {
+ // Use the data in your JavaScript code
+});
+pythonProcess.stderr.on('data', (data) => {
+ console.error(data);
+});
+pythonProcess.on('close', (code) => {
+ if (code !== 0) {
+   console.error(`Python process exited with code ${code}`);
+ }
+});
